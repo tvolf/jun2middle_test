43dbc9de86998c20c05104791811b8ffc4294315
Stan Gritsyuk <gtvolf@gmail.com>
2020-09-15 22:59:42 +0300

    Added controller classes

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UploadProductsRequest;
+use App\Jobs\GenerateYmlJob;
+use App\Models\Job;
+use Illuminate\Support\Str;
+
+class ProductController extends Controller
+{
+    public function store(UploadProductsRequest $request)
+    {
+        $products = $request->input('items');
+
+        $fileName = Str::random(20) . '.yml';
+
+        $job = Job::query()->create(
+            [
+                'filename' => $fileName,
+                'status' => Job::STATUS_NEW
+            ]
+        );
+
+        GenerateYmlJob::dispatch($job, collect($products));
+
+        return response()->json(['message' => 'Task to generate YML file started.']);
+    }
+}
