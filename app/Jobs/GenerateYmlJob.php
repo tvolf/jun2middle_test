4c1308bdbf76c50c8d2ae765f8848ca8f9ccb09f
Stan Gritsyuk <gtvolf@gmail.com>
2020-09-18 11:55:15 +0300

    Added base code to generate YML files in GenerateYmlJob class

--- app/Jobs/GenerateYmlJob.php
+++ app/Jobs/GenerateYmlJob.php
@@ -3,6 +3,9 @@
 namespace App\Jobs;
 
 use App\Models\Job;
+use Bukashk0zzz\YmlGenerator\Generator;
+use Bukashk0zzz\YmlGenerator\Model\ShopInfo;
+use Bukashk0zzz\YmlGenerator\Settings;
 use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -10,6 +13,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
 
 class GenerateYmlJob implements ShouldQueue
 {
@@ -37,13 +41,42 @@ class GenerateYmlJob implements ShouldQueue
      */
     public function handle()
     {
+
         try {
+            $file = Storage::disk('public')->path('ymls/' .  $this->passedJob->getFilename());
+
+            $settings = (new Settings())
+                ->setOutputFile($file)
+                ->setEncoding('UTF-8')
+            ;
+
+// Creating ShopInfo object (https://yandex.ru/support/webmaster/goods-prices/technical-requirements.xml#shop)
+            $shopInfo = (new ShopInfo())
+                ->setName('BestShop')
+                ->setCompany('Best online seller Inc.')
+                ->setUrl('http://www.best.seller.com/')
+            ;
+
+
+            $currencies = [];
+            $categories = [];
+            $offers = [];
+            $deliveries = [];
+
             $this->products->each(
                 function (array $item) {
                     $this->passedJob->setStatus(Job::STATUS_IN_PROGRESS)
                         ->save();
 //                ...  here is processing of current record
                 }
+            );
+
+            (new Generator($settings))->generate(
+                $shopInfo,
+                $currencies,
+                $categories,
+                $offers,
+                $deliveries
             );
 
             $this->passedJob->setStatus(Job::STATUS_SUCCESS)
