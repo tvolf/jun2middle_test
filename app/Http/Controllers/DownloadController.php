f1a82e58eeaf9c20e430453f77fe066b805435a2
Stan Gritsyuk <gtvolf@gmail.com>
2020-09-18 13:27:44 +0300

    Added getFile() method to DownloadController class

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -2,7 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
 class DownloadController extends Controller
 {
-    //
+    /**
+     * @param string $url
+     * @return JsonResponse|BinaryFileResponse
+     */
+    public function getFile(string $url)
+    {
+        /** @var Job $job */
+        $job = Job::query()->where('filename', $url)->firstOrFail();
+
+        $status = $job->getStatus();
+
+        if ($status === Job::STATUS_SUCCESS) {
+            $fileName = Storage::disk('public')->path('ymls/' . $job->getFilename());
+            return response()->download($fileName);
+        }
+
+        $responseData = [
+            'status' => $status
+        ];
+
+        if ($status === Job::STATUS_FAILED) {
+            $responseData['message'] = $job->getError();
+        }
+
+        return response()->json($responseData);
+    }
 }
