22349261e81eacce9ec35fba40b3491a9ec976cd
Stan Gritsyuk <gtvolf@gmail.com>
2020-09-18 14:05:14 +0300

    Updated TestCase class to remove temporary files created while testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,23 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected array $filesToDelete;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->filesToDelete = [];
+    }
+
+    /**
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        foreach ($this->filesToDelete as $filename) {
+            @unlink($filename);
+        }
+    }
 }
